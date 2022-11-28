2cf2bafeaf77c61d46afb4f7d11c97ddd2bdbdc6
JSkrzynski <76210734+JSkrzynski@users.noreply.github.com>
2022-11-28 01:20:05 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "assets"),
         filename: "bundle.js",
-        publicPath: "/7zad2/",
+        publicPath: "/7zad2tfn/",
         library: 'webpackGhPages'
     },
     module: {
